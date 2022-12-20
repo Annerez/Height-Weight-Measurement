a7f7d8c2e65cdee25ded02e6432f348e45471f04
Annerez <98201562+Annerez@users.noreply.github.com>
2022-12-20 21:48:55 +0700

    Update height.ino

--- height.ino
+++ height.ino
@@ -1,8 +1,8 @@
-#include <LiquidCrystal.h> //Please replace the single quote characters ('') with the parenthesis character (<>)
+#include <LiquidCrystal.h>
 #include "HX711.h"
 
-float calibration_factor =34779.00; 
-#define zero_factor 8535481
+float calibration_factor = 1.00; //your calibration factor
+#define zero_factor //Your zero factor
 #define DOUT  A3
 #define CLK   A2
 #define DEC_POINT  2
@@ -11,7 +11,7 @@ float offset=0;
 float get_units_kg();
 
 HX711 scale;
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
 
 const int trigPin = 9;
 const int echoPin = 10;
@@ -25,7 +25,7 @@ Serial.begin(115200);
 Serial.println("Load Cell");
 scale.set_scale(calibration_factor); 
 scale.set_offset(zero_factor);
-lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+lcd.begin(16,2);
 pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);
 
@@ -44,9 +44,9 @@ duration = pulseIn(echoPin, HIGH);
 distanceCm= duration*0.034/2;
 String data = String(get_units_kg()+offset, DEC_POINT);
 
-lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
-lcd.print("Your Height: "); // Prints string "Distance" on the LCD
-lcd.print(x - distanceCm); // Prints the distance value from the sensor
+lcd.setCursor(0,0); 
+lcd.print("Your Height: ");
+lcd.print(x - distanceCm); 
 lcd.print("  cm");
 lcd.setCursor(0,1);
 lcd.print("Your Weight: "); 
